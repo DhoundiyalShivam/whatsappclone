5a69f507269488d8ac803d51d0ad8aa1d93ac24a
DhoundiyalShivam <shivamdhoundiyal2@gmail.com>
2022-02-22 00:44:56 +0530

    start

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+//This is to connect the database with the our app
+const firebaseConfig = {
+    apiKey: "AIzaSyCAbQjPoJh3QuLcnubpN-WRMacedTyNVuU",
+    authDomain: "whats-app-clone-91040.firebaseapp.com",
+    projectId: "whats-app-clone-91040",
+    storageBucket: "whats-app-clone-91040.appspot.com",
+    messagingSenderId: "889928404886",
+    appId: "1:889928404886:web:947ed10284cb15e4db17b7",
+    measurementId: "G-HH0R15VVQE"
+  };
