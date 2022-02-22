7b9233135aa261a64b091f235ecb318154c2096a
DhoundiyalShivam <shivamdhoundiyal2@gmail.com>
2022-02-22 18:59:37 +0530

    sidebarChatDone

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,45 @@
 import React from 'react'
 import './Sidebar.css'
-function Sidebar(){
-    return(
+import { Avatar, IconButton } from "@material-ui/core"
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
+function Sidebar() {
+    return (
         <div className='sidebar'>
-        <h1>SidebAr</h1>
+            {/* <h1>SidebAr</h1> */}
+            <div className='sidebarHeader'>
+                <Avatar />
+                <div className='sidebarHeaderRight'>
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className='sidebarSearch'>
+                <div className='sidebarSearchContainer'>
+                    <SearchOutlined />
+                    <input placeholder='Search or start new chat' type="text" />
+                </div>
+            </div>
+            <div className='sidebarChats'>
+                <SidebarChat addNewChat/>
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+            </div>
+
 
         </div>
-        )
+    )
 }
 export default Sidebar;
---
+++ src/SidebarChat.js
@@ -0,0 +1,29 @@
+import { Avatar } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import './sidebarChat.css'
+function SidebarChat({addNewChat}) {
+    console.log(addNewChat)
+    const [avatarImg,setAvatarImg]=useState('')
+    useEffect(()=>{
+        setAvatarImg(Math.floor(Math.random()*5000))
+    },[])
+    const createChat=()=>{
+        const roomName=prompt("please enter name for chat")
+        if(roomName){
+            console.log(roomName)
+        }
+    }
+    return !addNewChat?(
+        <div className='sidebarChat'>
+            <Avatar src={`https://avatars.dicebear.com/api/human/${avatarImg}.svg`}/> 
+            <div className='sidebarChatInfo'>
+                <h2>Room Name</h2>
+                <p>Last message...</p>
+            </div>
+        </div>
+    ):(<div onClick={createChat} className="sidebarChat"> 
+    <h2>Add new Chat</h2>
+    </div>)
+}
+export default SidebarChat
+//this above used api gives us the randon avatar image
